12fa8b6fc0770250bd442851f5e66ca0b2c3560e
ObikaBless <blesschinedumobika@gmail>
2023-09-15 15:36:53 +0100

    new update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -135,7 +135,7 @@ const Navbar = () => {
             </Link>
           </ul>
 
-          <a href="/assets/obika_resume.pdf" target="_blank">
+          <a href="/assets/myResume.pdf" target="_blank">
             <motion.button
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
